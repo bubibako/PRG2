c9a39b6ff0b59f136b7a5f22693b23529911bd40
Arthur <163989344+bubibako@users.noreply.github.com>
2024-09-12 17:41:15 +0300

    main.swift

--- PRG2/main.swift
+++ PRG2/main.swift
@@ -4,8 +4,60 @@
 //
 //  Created by Arthur Trampnau on 12/09/24.
 //
-
-import Foundation
-
-print("Hello, World!")
-
+print("Enter a number 1:")
+var a = readLine()
+print("Enter a number 2:")
+var b = readLine()
+print("Enter enter:")
+var ent = readLine()
+func plus(a: Int, b: Int) -> Int {
+    return a + b
+}
+func minus(a: Int, b: Int) -> Int {
+    return a - b
+}
+func multiply(a: Int, b: Int) -> Int {
+    return a * b
+}
+func divide(a: Int, b: Int) -> Int {
+    return a / b
+}
+if ent == "+" {
+    if
+        let a = a,
+        let b = b
+    {
+        let x = plus(a: (Int(a) ?? 0), b: (Int(b) ?? 0))
+        print(x)
+    }
+}
+if ent == "-" {
+    if
+        let a = a,
+        let b = b
+    {
+        let x = minus(a: (Int(a) ?? 0), b: (Int(b) ?? 0))
+        print(x)
+    }
+}
+if ent == "*" {
+    if
+        let a = a,
+        let b = b
+    {
+        let x = multiply(a: (Int(a) ?? 0), b: (Int(b) ?? 0))
+        print(x)
+    }
+}
+if ent == "/" {
+    if b == "0" {
+        print("Can't divide by zero")
+    }
+    else if
+        let a = a,
+        let b = b
+    {
+        let x = divide(a: (Int(a) ?? 0), b: (Int(b) ?? 0))
+        print(x)
+    }
+}
